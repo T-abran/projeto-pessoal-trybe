abed4a23ea10ce527faf92584e2407201f6c7c87
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-14 19:19:54 -0300

    requisito 1 e 2

---
+++ script.js
@@ -0,0 +1,9 @@
+function colorPalette() {
+    const palleta = document.querySelectorAll('.color');
+    let cores = ['black','green','red','yellow'];
+    for (let i = 0; i < palleta.length; 1 += 1){
+        palleta[i]
+    }
+}
+
+window.onload
