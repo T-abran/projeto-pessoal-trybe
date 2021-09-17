684c0ae6028e01ad5b4d3d5783a2ea6909c85daa
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-17 18:50:19 -0300

    requisito 8

--- script.js
+++ script.js
@@ -9,4 +9,37 @@ function adicionarClasse() {
     color[index].addEventListener('click', selecionar);
   }
 }
-window.onload = adicionarClasse;
+
+
+function pintar(origem) {
+  const corSelected = document.querySelector('.selected').style.backgroundColor;
+  origem.target.style.backgroundColor = corSelected;
+}
+function pegaSelecionar() {
+
+  const palleta = document.querySelectorAll('.color');
+
+  for (let index = 0; index < palleta.length; index += 1){
+    palleta[index].addEventListener('click', selecionar);
+}
+}
+function adicionarEventos() {
+ 
+  const pixel = document.querySelectorAll('.pixel');
+
+
+  for (let index = 0; index < pixel.length; index += 1){
+      pixel[index].addEventListener('click', pintar);
+ }
+}
+
+
+document.getElementById('yellow').style.backgroundColor = 'yellow';
+document.getElementById('black').style.backgroundColor = 'black';
+document.getElementById('pink').style.backgroundColor = 'pink';
+document.getElementById('blue').style.backgroundColor = 'blue';
+   
+window.onload =()=>{
+  pegaSelecionar()
+  adicionarEventos();
+}
